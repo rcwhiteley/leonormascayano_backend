ed9aa483195ea2c1a13b6ac4c95a96247cde2f2e
Rodrigo Córdova <r.a.c.whiteley@gmail.com>
2022-11-22 14:54:02 -0300

    deploy

---
+++ deploy.bat
@@ -0,0 +1,14 @@
+set frontPath=C:\Users\racwh\Desktop\intranet\intranet-leonormascayano
+set backPath=C:\Users\racwh\Desktop\intranet\backend-lmascayno\lmascayano-backend
+cd %frontPath%
+call npm run build
+xcopy /s %frontPath%\build\index.html %backPath%\resources\views\index.blade.php /Y
+xcopy /s %frontPath%\build\static %backPath%\public\static /Y
+cd %backPath%
+git add .
+git commit -m "deploy"
+git push origin master
+@echo off
+cls
+call rake
+pause
