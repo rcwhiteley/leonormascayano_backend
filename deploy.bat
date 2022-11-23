564ef242e42ce4f55aafc5e36e44729d1824e17c
Rodrigo Córdova <r.a.c.whiteley@gmail.com>
2022-11-23 01:27:19 -0300

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 set frontPath=C:\Users\racwh\Desktop\intranet\intranet-leonormascayano
 set backPath=C:\Users\racwh\Desktop\intranet\backend-lmascayno\lmascayano-backend
 cd %frontPath%
-
+call npm run build
 xcopy /s %frontPath%\build\index.html %backPath%\resources\views\index.blade.php /Y
 xcopy /s %frontPath%\build\static %backPath%\public\static /Y
 cd %backPath%
